f856fa30c539f1d960176ceeba141382ff9332ee
Julian Fernandez <julianfernandez1811@gmail.com>
2024-03-27 16:40:04 -0300

    add basic structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './shared/pages/home-page/home-page.component';
+import { AboutPageComponent } from './shared/pages/about-page/about-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomePageComponent
+  },
+  {
+    path: 'about',
+    component: AboutPageComponent
+  },
+  {
+    path: '**', // Cualquier dirección no contemplada redirige al home
+    redirectTo: 'home'
+  }
+]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    // forRoot hace referencia a que este archivo es la principal dirección de la app.
+    // Si se agrega otro root en un componente hijo, sería forChild()
+    RouterModule.forRoot(routes),
+  ],
+  exports: [
+    RouterModule // Para linkear con app-module despues
+  ]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+}
